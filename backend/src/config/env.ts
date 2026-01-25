865e24cc7888760b9256fff8cef39720c2146f9a
Yashu Youwaraj <amanyasu@gmail.com>
2026-01-25 17:32:27 +0530

    Initial Commit

---
+++ backend/src/config/env.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const EnvSchema = z.object({
+  PORT: z.string().default("5000"),
+  DB_HOST: z.string().default("localhost"),
+  DB_PORT: z.string().default("6450"),
+  DB_NAME: z.string().default("realtime_chat_and_threads_app"),
+  DB_USER: z.string().default("postgres"),
+  DB_PASSWORD: z.string().default("postgres"),
+});
+
+const parsed = EnvSchema.safeParse(process.env)
+
+if(!parsed.success){
+    process.exit(1)
+}
+
+export const env = parsed.data
